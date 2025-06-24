6bc0123e80a5c81b619a61389de1864497ebd22f
krissceron <krissceronb03@gmail.com>
2025-06-24 02:08:42 +0000

    Añadir checkstyle

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
             w.println("<h1>Hello from Java CI/CD, Kriss!</h1>");
-             w.println("<h2>Esto es un despliegue continuo</h2>");
+            w.println("<p><a href=\"checkstyle.html\">Ver reporte Checkstyle</a></p>");
             System.out.println("HTML generado correctamente.");
         } catch (IOException e) {
             e.printStackTrace();
