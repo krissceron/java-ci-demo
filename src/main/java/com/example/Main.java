a3e7516b9ddb6fd7a814c46f8cd5c450df14e060
krissceron <krissceronb03@gmail.com>
2025-06-24 01:58:48 +0000

    Se modificó la página

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
-            w.println("<h1>Hello from Java CI/CD!</h1>");
+            w.println("<h1>Hello from Java CI/CD, Kriss!</h1>");
+             w.println("<h2>Esto es un despliegue continuo</h2>");
             System.out.println("HTML generado correctamente.");
         } catch (IOException e) {
             e.printStackTrace();
